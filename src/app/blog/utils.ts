f139b737925a44b3424dc001d6fe608b0fea450e
JovannyRch <jovannyrch@gmail.com>
2023-07-25 12:27:59 -0600

    Auto-generated commit [ci skip]

--- src/app/blog/utils.ts
+++
@@ -1,8 +0,0 @@
-// utils.ts
-
-export function chunk<T>(array: T[], size: number): T[][] {
-    return Array.from({ length: Math.ceil(array.length / size) }, (v, index) =>
-      array.slice(index * size, index * size + size)
-    );
-  }
-  
